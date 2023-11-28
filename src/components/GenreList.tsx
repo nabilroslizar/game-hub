92664c0c330c4b9d87ea9b90ce589c2f74dfdb56
nabilroslizar95 <nabil.roslizar@gmail.com>
2023-11-28 23:40:38 +0800

    error genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,7 +3,9 @@ import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data, isLoading } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
   if (isLoading) return <Spinner />;
   return (
     <List>
